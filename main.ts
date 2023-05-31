7676be582e09f9f2ece7a8d5b1f2a9c7574410da
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-05-31 13:29:43 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,29 @@
 // jede po černý je zapotřebí udělat zatáčení 
 
-
+// asi by šlo udělat měření rychlost otačení kola / obvod kola 
 //piny
 const pinC = DigitalPin.P15
 const pinL = DigitalPin.P14
 const pinR = DigitalPin.P13
+
+const encoderL = DigitalPin.P12
+const encoderR = DigitalPin.P8
 //motory
 const m1 = PCAmotor.Motors.M1
 const m4 = PCAmotor.Motors.M4
+
 //hodnoty
 let whiteLine: number = 0
+const data: any[] = []
+
+
 
 pins.setPull(pinC, PinPullMode.PullNone)
 pins.setPull(pinL, PinPullMode.PullNone)
 pins.setPull(pinR, PinPullMode.PullNone)
 
+pins.setPull(encoderL, PinPullMode.PullNone)
+pins.setPull(encoderR, PinPullMode.PullNone)
 function car_motor(lw: number = 0, rw: number = 0) {
     const ul = Math.map(lw, -100, 100, -255, 255)
     const ur = Math.map(rw, -100, 100, -215, 215)
@@ -24,13 +33,29 @@ function car_motor(lw: number = 0, rw: number = 0) {
 }
 
 
+function history(c :boolean,l: boolean,r:boolean){
+    data.push({c,l,r})
+
+    if(data.length > 9){
+        data.pop()
+    }
+}
+
+
 basic.forever(function () {
     let c: boolean = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true ;
     let l: boolean = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true;
     let r: boolean = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true;
 
+    
+    basic.pause(100)
+    console.log(encoderL)
+    console.log(encoderR)
+
     basic.pause(100)
     
+
+    // rovná jízda po čáře
     if (c === true || l === true || r === true){
         car_motor(-200,100)
     }else{
