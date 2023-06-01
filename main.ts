a0721c9c47ea3b7125b128cad33aa98b19941998
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-06-01 11:31:12 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // jede po černý je zapotřebí udělat zatáčení 
 
-// asi by šlo udělat měření rychlost otačení kola / obvod kola 
+
 //piny
 const pinC = DigitalPin.P15
 const pinL = DigitalPin.P14
@@ -13,7 +13,7 @@ const m4 = PCAmotor.Motors.M4
 
 //hodnoty
 let whiteLine: number = 0
-const data: any[] = []
+let data: any[] = []
 
 
 
@@ -21,8 +21,6 @@ pins.setPull(pinC, PinPullMode.PullNone)
 pins.setPull(pinL, PinPullMode.PullNone)
 pins.setPull(pinR, PinPullMode.PullNone)
 
-pins.setPull(encoderL, PinPullMode.PullNone)
-pins.setPull(encoderR, PinPullMode.PullNone)
 function car_motor(lw: number = 0, rw: number = 0) {
     const ul = Math.map(lw, -100, 100, -255, 255)
     const ur = Math.map(rw, -100, 100, -215, 215)
@@ -41,22 +39,35 @@ function history(c :boolean,l: boolean,r:boolean){
 }
 
 
+
+
+
 basic.forever(function () {
     let c: boolean = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true ;
     let l: boolean = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true;
     let r: boolean = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true;
 
-    
-    
 
+    
     basic.pause(100)
     
 
     // rovná jízda po čáře
-    if (c === true || l === true || r === true){
-        car_motor(-200,100)
+    if (c === true && l === true && r === true || c === true && l != true && r != true){
+        car_motor(-125,75)
+    }else if (c === true && l === true && r === false){
+        car_motor(-75, 100)
+    }else if(c === true && l === false && r === true){
+        car_motor(-200,75)
+    } else if (c === false && l === false && r === true){
+        car_motor(-200, 75)
+    } else if(c === true && l === true && r === false){
+        car_motor(-75, 100)
     }else{
         car_motor(0,0)
     }
 
 })
+
+
+
