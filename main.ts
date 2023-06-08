24f66484f83b6d8cbc0de6db6b6eba8ccc81ce00
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-06-08 11:28:53 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -13,7 +13,9 @@ const m4 = PCAmotor.Motors.M4
 
 //hodnoty
 let whiteLine: number = 0
-let data: any[] = []
+let dataL: any[] = []
+let dataC: number[] = []
+let dataR: any[] = []
 
 
 
@@ -30,13 +32,128 @@ function car_motor(lw: number = 0, rw: number = 0) {
 }
 
 
-function history(c :boolean,l: boolean,r:boolean){
-    data.push({c,l,r})
 
-    if(data.length > 9){
-        data.pop()
+
+function isOnLineC(){
+    let value: number = 0
+      
+    for (let i = 0; i < 5; i++) {
+        value += dataC[i]
+    }
+
+    if (Math.round(value / 5) === 1) {
+        return false
+    } else {
+        return true
     }
 }
+
+
+function isOnLineL() {
+    let value: number = 0
+    
+    for (let i = 0; i < 5; i++) {
+        value += dataL[i]
+    }
+
+    if (Math.round(value / 5) === 1) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+function isOnLineR() {
+    let value: number = 0
+
+    for (let i = 0; i < 5; i++) {
+        value += dataR[i]
+    }
+
+    if (Math.round(value / 5) === 1){
+        return false
+    }else{
+        return true
+    }
+
+
+}
+
+
+function appendValuesC(c : boolean){
+    let count: number = 0
+    let convertC: number = 0
+
+    while (count <= 5) {
+        if (c === true) {
+            convertC = 1
+            dataC.push(convertC)
+        } else {
+            convertC = 0
+            dataC.push(convertC)
+        }
+
+
+        if (dataC.length > 5) {
+            dataC.pop()
+        }
+
+        count++
+    }
+    return dataC
+}
+
+
+
+function appendValuesL(l: boolean) {
+    let count: number = 0
+    let convertL: number = 0
+
+    while (count <= 5) {
+        if (l === true) {
+            convertL = 1
+            dataL.push(convertL)
+        } else {
+            convertL = 0
+            dataL.push(convertL)
+        }
+
+
+        if (dataL.length > 5) {
+            dataL.pop()
+        }
+
+        count++
+    }
+    return dataL
+}
+
+
+function appendValuesR(r: boolean) {
+    let count: number = 0
+    let convertR: number = 0
+
+    while (count <= 5) {
+        if (r === true) {
+            convertR = 1
+            dataL.push(convertR)
+        } else {
+            convertR = 0
+            dataR.push(convertR)
+        }
+
+
+        if (dataR.length > 5) {
+            dataR.pop()
+        }
+
+        count++
+    }
+    return dataR
+}
+
+
 
 
 
@@ -47,26 +164,37 @@ basic.forever(function () {
     let l: boolean = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true;
     let r: boolean = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true;
 
-
-    
-    basic.pause(100)
+    appendValuesC(c)
+    appendValuesL(l)
+    appendValuesR(r)
     
 
     // rovná jízda po čáře
     if (c === true && l === true && r === true || c === true && l != true && r != true){
         car_motor(125,65)
-    }else if (c === true && l === true && r === false){
-        car_motor(100, 65)
-    }else if(c === true && l === false && r === true){
-        car_motor(100,65)
+    }else if (c === true && l === false && r === false){
+        car_motor(125, 65)
+    }else if(c === true && l === true && r === false){
+        car_motor(50,65)
+    } else if (c === false && l === true && r === false){
+        car_motor(50, 65)
+    } else if(c === true && l === false && r === true){
+        car_motor(125, 26)
+
     } else if (c === false && l === false && r === true){
-        car_motor(125, 50)
-    } else if(c === true && l === true && r === false){
-        car_motor(125, 50)
+        car_motor(125, 26)
     }else{
-        car_motor(0,0)
+       c = isOnLineC()
+       l = isOnLineL()
+       r = isOnLineR()
+
+       if(c === true && l === true && r === true){
+           car_motor(0,0)
+       }
     }
 
+    
+    
 })
 
 
