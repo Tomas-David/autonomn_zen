dbbe14a7e3d3d4ad7b0f557798d5b028e0d8e259
Tomáš David <115162566+Tomas-David@users.noreply.github.com>
2023-06-15 11:47:45 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-// jede po černý je zapotřebí udělat zatáčení 
-
+radio.setGroup(14)
 
 //piny
 const pinC = DigitalPin.P15
 const pinL = DigitalPin.P14
 const pinR = DigitalPin.P13
-
 
 //motory
 const m1 = PCAmotor.Motors.M1
@@ -13,12 +11,11 @@ const m4 = PCAmotor.Motors.M4
 
 //hodnoty
 let whiteLine: number = 0
-let dataL: any[] = []
+let dataL: number[] = []
 let dataC: number[] = []
-let dataR: any[] = []
-
-
-
+let dataR: number[] = []
+let turn = false
+let count = 0
 pins.setPull(pinC, PinPullMode.PullNone)
 pins.setPull(pinL, PinPullMode.PullNone)
 pins.setPull(pinR, PinPullMode.PullNone)
@@ -28,17 +25,14 @@ function car_motor(lw: number = 0, rw: number = 0) {
     const ur = Math.map(rw, -100, 100, -215, 215)
     PCAmotor.MotorRun(PCAmotor.Motors.M1, ur)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -ul)
-
 }
 
 
-
-
-function isOnLineC() {
+function isOnLine(data: any[]) {
     let value: number = 0
 
     for (let i = 0; i < 3; i++) {
-        value += dataC[i]
+        value += data[i]
     }
 
     if (Math.round(value / 3) === 1) {
@@ -46,46 +40,12 @@ function isOnLineC() {
     } else {
         return true
     }
-}
-
-
-function isOnLineL() {
-    let value: number = 0
-
-    for (let i = 0; i < 3; i++) {
-        value += dataL[i]
-    }
-
-    if (Math.round(value / 3) === 1) {
-        return false
-    } else {
-        return true
-    }
-}
-
-
-function isOnLineR() {
-    let value: number = 0
-
-    for (let i = 0; i < 3; i++) {
-        value += dataR[i]
-    }
-
-    if (Math.round(value / 3) === 1) {
-        return false
-    } else {
-        return true
-    }
-
-
 }
 
 
 function appendValuesC(c: boolean) {
-    let count: number = 0
-    let convertC: number = 0
+      let convertC: number = 0
 
-    while (count <= 3) {
         if (c === true) {
             convertC = 1
             dataC.push(convertC)
@@ -94,23 +54,17 @@ function appendValuesC(c: boolean) {
             dataC.push(convertC)
         }
 
-
         if (dataC.length > 3) {
             dataC.pop()
         }
-
-        count++
-    }
     return dataC
 }
 
 
 
 function appendValuesL(l: boolean) {
-    let count: number = 0
     let convertL: number = 0
 
-    while (count <= 3) {
         if (l === true) {
             convertL = 1
             dataL.push(convertL)
@@ -119,22 +73,15 @@ function appendValuesL(l: boolean) {
             dataL.push(convertL)
         }
 
-
         if (dataL.length > 3) {
             dataL.pop()
         }
-
-        count++
-    }
     return dataL
 }
 
 
 function appendValuesR(r: boolean) {
-    let count: number = 0
     let convertR: number = 0
-
-    while (count <= 3) {
         if (r === true) {
             convertR = 1
             dataL.push(convertR)
@@ -142,75 +89,78 @@ function appendValuesR(r: boolean) {
             convertR = 0
             dataR.push(convertR)
         }
-
-
         if (dataR.length > 3) {
             dataR.pop()
         }
-
-        count++
-    }
     return dataR
 }
 
-let turn = false
+
 radio.onReceivedNumber(function (receivedNumber: number) {
-    
     switch (receivedNumber) {
         case 1:
             turn = true
-            basic.pause(200)
+            basic.pause(500)
             car_motor(0,90)
             basic.pause(500)
             turn = false
             break
         case 2:
             turn = true
-            basic.pause(200)
-            car_motor(90, 0)
             basic.pause(500)
+            car_motor(90,0)
+            basic.pause(500)
+            turn = false
+            break
+        case 3:
+            turn = true
+            basic.pause(100)
+            car_motor(125,65)
             turn = false
             break
     }
 })
-
-
 
 basic.forever(function () {
     let c: boolean = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? false : true;
     let l: boolean = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? false : true;
     let r: boolean = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? false : true;
 
-    appendValuesC(c)
-    appendValuesL(l)
-    appendValuesR(r)
+    
 
-if(turn === true){
-    // rovná jízda po čáře
-    if (c === true && l === true && r === true || c === true && l != true && r != true) {
-        car_motor(125, 65)
-    } else if (c === true && l === false && r === false) {
-        car_motor(125, 65)
-    } else if (c === true && l === true && r === false) {
-        car_motor(50, 65)
-    } else if (c === false && l === true && r === false) {
-        car_motor(50, 65)
-    } else if (c === true && l === false && r === true) {
-        car_motor(125, 40)
 
-    } else if (c === false && l === false && r === true) {
-        car_motor(125, 40)
-    } else {
-        c = isOnLineC()
-        l = isOnLineL()
-        r = isOnLineR()
+    if(turn === false){
 
-        if (c === false && l === false && r === false) {
-            car_motor(0, 0)
+        while(count <= 5) {
+            appendValuesC(c)
+            appendValuesL(l)
+            appendValuesR(r)
+            count++
         }
+        
+        // rovná jízda po čáře
+        
+        if (c === true && l === true && r === true || c === true && l != true && r != true){
+            car_motor(125,65)
+        }else if (c === true && l === false && r === false){
+            car_motor(125, 65)
+        }else if(c === true && l === true && r === false){
+            car_motor(50,65)
+        } else if (c === false && l === true && r === false){
+            car_motor(50, 65)
+        } else if(c === true && l === false && r === true){
+            car_motor(125, 10)
+        } else if (c === false && l === false && r === true){
+            car_motor(125, 10)
+        }else{
+            c = isOnLine(dataC)
+            l = isOnLine(dataL)
+            r = isOnLine(dataR)
 
+            if(c === true && l === true && r === true){
+            car_motor(0,0)
+            }
+        }
     }
-}
-   
     
 })
